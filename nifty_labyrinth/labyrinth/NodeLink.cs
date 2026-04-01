77ef0442a576d5104b115c0c3653c6e88d8223c5
Daniel Payne <danielhpayne7@gmail.com>
2026-03-31 23:13:21 -0600

    MermaidJS diagram

--- nifty_labyrinth/labyrinth/NodeLink.cs
+++ nifty_labyrinth/labyrinth/NodeLink.cs
@@ -3,6 +3,4 @@ namespace labyrinth;
 public record NodeLink(MazeCell From, string Direction, MazeCell To)
 {
     public string Label => $"{From.Id}[\"{From}\"]--{Direction}-->{To.Id}[\"{To}\"]";
-
-
 }
